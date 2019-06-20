f1891ce2d0b6e778bbeb4c1daee68d0cfc3c0c6f
tacosontherun <52044813+tacosontherun@users.noreply.github.com>
2019-06-20 13:42:30 -0700

    Create str_ex1.py

---
+++ str_ex1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+name1 = dave
+name2 = kurt
+name3 = krist
+name4 = input("Enter fourth name: ")
+
+print()
+print("{:>30}".format(name1))
+print("{:>30}".format(name2))
+print("{:>30}".format(name3))
+print("{:>30}".format(name4))
+print()
